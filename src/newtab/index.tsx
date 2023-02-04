1b583fa5fb94d89835cc001fde5ec0b0c772b312
phillc <15082+phillc@users.noreply.github.com>
2023-02-03 22:09:29 -0500

    Add chart

--- src/newtab/index.tsx
+++ src/newtab/index.tsx
@@ -1,11 +1,36 @@
 import { useState, useEffect } from "react"
 import "../style.css"
 
+import {
+  Chart as ChartJS,
+  ChartData,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const chartOptions = {
+  responsive: true,
+};
+
 function IndexNewtab() {
   const [tabsNow, setTabsNow] = useState<any>()
-  const [tabsHistory, setTabsHistory] = useState<any>()
   const [windowsNow, setWindowsNow] = useState<any>()
-  const [windowsHistory, setWindowsHistory] = useState<any>()
+  const [dateData, setDateData] = useState<any>()
   useEffect(() => {
     const fetch = async () => {
       const windows = await chrome.windows.getAll({populate: true});
@@ -13,17 +38,26 @@ function IndexNewtab() {
       const tabs = await chrome.tabs.query({windowType:'normal'});
       setTabsNow(tabs);
 
-      const data = JSON.parse(localStorage.getItem('app')) || {tabs: {}, windows: {}};
+      const data = JSON.parse(localStorage.getItem('app')) || {};
 
-      data.tabs[today] = Math.max(tabs.length, data.tabs[today]);
-      data.windows[today] = Math.max(windows.length, data.tabs[today]);
+      const today = new Date().toJSON().slice(0, 10);
+      data[today] = data[today] || {maxTabs: 0, maxWindows: 0}
+      data[today].maxTabs = Math.max(tabs.length, data[today].maxTabs);
+      data[today].maxWindows = Math.max(windows.length, data[today].maxWindows);
+
       localStorage.setItem('app', JSON.stringify(data));
-      setTabsHistory(data.tabs);
-      setWindowsHistory(data.windows);
+      setDateData(data);
     }
     fetch();
-  });
-  const today = new Date().toJSON().slice(0, 10);
+  }, []);
+
+  const chartData: ChartData = dateData && Object.keys(dateData).length > 1 && {
+    labels: Object.keys(dateData),
+    datasets: [{
+      label: "tabs",
+      data: Object.values(dateData).map((v: any) => v.maxTabs)
+    }]
+  }
 
   const badge = (title, collection) => {
     if(collection && collection.length) {
@@ -44,11 +78,17 @@ function IndexNewtab() {
             {badge("tabs", tabsNow)}
           </div>
         </div>
-        <div className="text-center flex flex-col justify-center items-center">across</div>
+        <div className="col-span-1 text-center flex flex-col justify-center items-center">across</div>
         <div className="col-span-3">
           <div className="shadow-xl w-64 h-64 bg-gradient-to-b from-blue-500 to-cyan-500">
             {badge("windows", windowsNow)}
           </div>
+        </div>
+        <div className="mt-8 col-start-3 col-span-3">
+          {chartData &&
+            <Line options={chartOptions} data={chartData} />
+            // JSON.stringify(chartData)
+          }
         </div>
       </div>
     </div>
