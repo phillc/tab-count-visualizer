89965a02cfb9e663356630cf082e03c36264dfdd
phillc <15082+phillc@users.noreply.github.com>
2023-02-03 22:09:29 -0500

    Add tailwind

---
+++ src/newtab/index.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import "../style.css"
+
+function IndexNewtab() {
+  const [data, setData] = useState("")
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
+      <h1>
+        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!sss
+      </h1>
+      <input onChange={(e) => setData(e.target.value)} value={data} />
+    </div>
+  )
+}
+
+export default IndexNewtab
