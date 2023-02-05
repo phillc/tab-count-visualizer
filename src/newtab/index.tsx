064a24b124cabf8c4e4c097f431a53d1b22bf7e7
phillc <15082+phillc@users.noreply.github.com>
2023-02-05 07:15:32 -0500

    Better graph

--- src/newtab/index.tsx
+++ src/newtab/index.tsx
@@ -12,8 +12,10 @@ import {
   Title,
   Tooltip,
   Legend,
+  TimeSeriesScale,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
+import 'chartjs-adapter-date-fns';
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -21,11 +23,47 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  TimeSeriesScale,
 );
 
 export const chartOptions = {
   responsive: true,
+  interaction: {
+    mode: 'index' as const,
+    intersect: false,
+  },
+  plugins: {
+    title: {
+      display: true,
+      text: 'Maximum per day',
+    },
+  },
+  scales: {
+    x: {
+        type: 'timeseries',
+        time: {
+            unit: 'day',
+            tooltipFormat: 'yyyy-MM-dd',
+        }
+    },
+    y1: {
+      type: 'linear' as const,
+      display: true,
+      position: 'left' as const,
+    },
+    y2: {
+      type: 'linear' as const,
+      display: true,
+      position: 'right' as const,
+      grid: {
+        drawOnChartArea: false,
+      },
+      ticks: {
+        precision: 0,
+      },
+    },
+  },
 };
 
 function IndexNewtab() {
@@ -50,6 +88,23 @@ function IndexNewtab() {
 
       const storage = new Storage();
       const data = await storage.get('history') || {};
+      // console.log(">>>>", data)
+      // data["2023-01-14"] = {
+      //   maxTabs: 132,
+      //   maxWindows: 4
+      // }
+      // data["2023-02-02"] = {
+      //   maxTabs: 145,
+      //   maxWindows: 2
+      // }
+      // data["2023-02-03"] = {
+      //   maxTabs: 152,
+      //   maxWindows: 2
+      // }
+      // data["2023-02-04"] = {
+      //   maxTabs: 162,
+      //   maxWindows: 3
+      // }
       const today = new Date().toJSON().slice(0, 10);
       data[today] = data[today] || {maxTabs: 0, maxWindows: 0}
       data[today].maxTabs = Math.max(tabs.length, data[today].maxTabs);
@@ -61,12 +116,32 @@ function IndexNewtab() {
     fetch();
   }, []);
 
-  const chartData: ChartData = history && Object.keys(history).length > 1 && {
-    labels: Object.keys(history),
-    datasets: [{
-      label: "tabs",
-      data: Object.values(history).map((v: any) => v.maxTabs)
-    }]
+  let labels;
+  let chartData: ChartData;
+  if (history && Object.keys(history).length > 1) {
+    labels = Object.keys(history).sort();
+
+    chartData = {
+      labels: labels,
+      datasets: [
+        {
+          label: "tabs",
+          data: labels.map((label: any) => history[label].maxTabs),
+          borderColor: 'rgb(59, 130, 246)',
+          backgroundColor: 'rgb(59, 130, 246, 0.5)',
+          yAxisID: 'y1',
+          tension: 0.1,
+        },
+        {
+          label: "windows",
+          data: labels.map((label: any) => history[label].maxWindows),
+          borderColor: 'rgb(6, 182, 212)',
+          backgroundColor: 'rgb(6, 182, 212, 0.5)',
+          yAxisID: 'y2',
+          tension: 0.1,
+        }
+      ]
+    }
   }
 
   const badge = (title, collection) => {
@@ -103,9 +178,8 @@ function IndexNewtab() {
                 style={{ 
                   backgroundImage: `url("${tab.favIconUrl}")`
                 }}
-                className="p-0.5 pl-7 font-medium hover:underline bg-slate-200 rounded-full bg-no-repeat bg-left bg-contain"
+                className="p-0.5 pl-7 pr-2 font-medium hover:underline bg-slate-200 rounded-full bg-no-repeat bg-left bg-contain"
               >
-                {/* { tab.favIconUrl && <img src={tab.favIconUrl} /> } */}
                 {tab.title}
               </a>
             </li>
